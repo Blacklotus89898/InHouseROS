0a62c49ef3fe4134fdb27115363fdfab44a7cd95
Blacklotus89898 <song.yang.chen89898@gmail.com>
2025-06-10 22:31:50 -0400

    oop webrtc

--- src/webrtc/callee.cpp
+++ src/webrtc/callee.cpp
@@ -11,165 +11,36 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-const int SIGNALING_PORT = 9000;
-
-const char* stateToString(rtc::PeerConnection::State state) {
-    switch (state) {
-        case rtc::PeerConnection::State::New: return "New";
-        case rtc::PeerConnection::State::Connecting: return "Connecting";
-        case rtc::PeerConnection::State::Connected: return "Connected";
-        case rtc::PeerConnection::State::Disconnected: return "Disconnected";
-        case rtc::PeerConnection::State::Failed: return "Failed";
-        case rtc::PeerConnection::State::Closed: return "Closed";
-        default: return "Unknown";
-    }
-}
-
-bool sendMessage(int sock, const std::string& msg) {
-    uint32_t len = htonl(msg.size());
-    if (send(sock, &len, sizeof(len), 0) != sizeof(len)) return false;
-    if (send(sock, msg.data(), msg.size(), 0) != (ssize_t)msg.size()) return false;
-    return true;
-}
-
-bool receiveMessage(int sock, std::string& out) {
-    uint32_t len = 0;
-    ssize_t n = recv(sock, &len, sizeof(len), MSG_WAITALL);
-    if (n == 0) return false;
-    if (n != sizeof(len)) return false;
-    len = ntohl(len);
-    out.resize(len);
-    n = recv(sock, &out[0], len, MSG_WAITALL);
-    if (n != (ssize_t)len) return false;
-    return true;
-}
-
-int main() {
-    try {
+class WebRTCCallee {
+public:
+    WebRTCCallee(int signalingPort = 9000)
+        : SIGNALING_PORT(signalingPort), connected(false), dataChannelReady(false), answerReady(false), server_sock(-1), sock(-1) {
         rtc::InitLogger(rtc::LogLevel::Info);
 
-        rtc::Configuration config;
         config.iceServers.emplace_back("stun:stun.l.google.com:19302");
         config.portRangeBegin = 10000;
         config.portRangeEnd = 10100;
 
-        auto callee = std::make_shared<rtc::PeerConnection>(config);
+        pc = std::make_shared<rtc::PeerConnection>(config);
+        setupPeerConnection();
+    }
 
-        std::mutex mtx;
-        std::condition_variable cv;
-        std::atomic<bool> connected{false};
-        std::atomic<bool> dataChannelReady{false};
-        std::atomic<bool> answerReady{false};
+    ~WebRTCCallee() {
+        close(sock);
+        close(server_sock);
+        if (signalingThread.joinable()) signalingThread.join();
+        if (pc) pc->close();
+    }
 
-        int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_sock < 0) {
-            perror("socket");
-            return 1;
-        }
+    void run() {
+        setupSignalingServer();
+        signalingThread = std::thread([this]() { signalingLoop(); });
 
-        sockaddr_in addr{};
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = INADDR_ANY;
-        addr.sin_port = htons(SIGNALING_PORT);
-
-        if (bind(server_sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
-            perror("bind");
-            return 1;
-        }
-
-        if (listen(server_sock, 1) < 0) {
-            perror("listen");
-            return 1;
-        }
-
-        std::cout << "[Callee] Waiting for caller connection..." << std::endl;
-        int sock = accept(server_sock, nullptr, nullptr);
-        if (sock < 0) {
-            perror("accept");
-            return 1;
-        }
-        std::cout << "[Callee] Caller connected." << std::endl;
-
-        callee->onLocalCandidate([&](rtc::Candidate candidate) {
-            std::string candStr = std::string(candidate);
-            std::string msg = "candidate:" + candStr;
-            sendMessage(sock, msg);
-            std::cout << "[Callee] Sent ICE candidate to caller." << std::endl;
-        });
-
-        callee->onStateChange([&](rtc::PeerConnection::State state) {
-            std::cout << "[Callee] State changed: " << stateToString(state) << std::endl;
-            connected = (state == rtc::PeerConnection::State::Connected);
-            cv.notify_all();
-        });
-
-        callee->onLocalDescription([&](rtc::Description description) {
-            std::string answerSdp = std::string(description);
-            std::string msg = "answer:" + answerSdp;
-            sendMessage(sock, msg);
-            std::cout << "[Callee] Sent answer SDP to caller." << std::endl;
-            answerReady = true;
-            cv.notify_all();
-        });
-
-        std::shared_ptr<rtc::DataChannel> dc;
-
-        callee->onDataChannel([&](std::shared_ptr<rtc::DataChannel> channel) {
-            std::cout << "[Callee] Data channel received." << std::endl;
-            dc = channel;
-
-            dc->onOpen([&]() {
-                std::cout << "[Callee] Data channel open." << std::endl;
-                dataChannelReady = true;
-                cv.notify_all();
-            });
-
-            dc->onMessage([&](rtc::message_variant msg) {
-                std::visit([&](auto&& data) {
-                    using T = std::decay_t<decltype(data)>;
-                    if constexpr(std::is_same_v<T, std::string>) {
-                        std::cout << "[Callee] Received message: " << data << std::endl;
-            
-                        std::string response = "Callee echo: " + data;
-            
-                        // Send it back
-                        if (dc && dc->isOpen()) {
-                            dc->send(response);
-                            std::cout << "[Callee] Sent transformed message: " << response << std::endl;
-                        }
-                    }
-                        
-                    else if constexpr(std::is_same_v<T, rtc::binary>)
-                        std::cout << "[Callee] Received binary message, size: " << data.size() << std::endl;
-                }, msg);
-            });
-        });
-
-        // Thread to receive signaling messages from caller
-        std::thread signalingThread([&]() {
-            std::string msg;
-            while (receiveMessage(sock, msg)) {
-                if (msg.rfind("offer:", 0) == 0) {
-                    std::string sdp = msg.substr(6);
-                    rtc::Description desc(sdp, rtc::Description::Type::Offer);
-                    callee->setRemoteDescription(desc);
-                    std::cout << "[Callee] Received and set offer SDP." << std::endl;
-                    // Now create answer
-                    callee->setLocalDescription();
-                } else if (msg.rfind("candidate:", 0) == 0) {
-                    std::string candStr = msg.substr(10);
-                    rtc::Candidate candidate(candStr);
-                    callee->addRemoteCandidate(candidate);
-                    std::cout << "[Callee] Received and added ICE candidate." << std::endl;
-                }
-            }
-        });
-
-        // Wait for connection
         {
             std::unique_lock<std::mutex> lock(mtx);
             if (!cv.wait_for(lock, std::chrono::seconds(30), [&]() { return connected.load(); })) {
                 std::cerr << "[Callee] Timeout waiting for connection." << std::endl;
+                return;
             }
         }
 
@@ -182,27 +53,151 @@ int main() {
                 }
             }
 
-            std::this_thread::sleep_for(std::chrono::seconds(10)); 
+            std::this_thread::sleep_for(std::chrono::seconds(10));
 
-            if (dataChannelReady) {
-                // Send some test messages
-                for (int i = 0; i < 5; i++) {
-                    if (dc && dc->isOpen()) {
-                        std::string msg = "Hello from callee #" + std::to_string(i);
-                        dc->send(msg);
-                        std::cout << "[Callee] Sent: " << msg << std::endl;
-                        std::this_thread::sleep_for(std::chrono::seconds(1));
-                    }
+            if (dataChannelReady && dc && dc->isOpen()) {
+                for (int i = 0; i < 5; ++i) {
+                    std::string msg = "Hello from callee #" + std::to_string(i);
+                    dc->send(msg);
+                    std::cout << "[Callee] Sent: " << msg << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
                 }
             }
         }
 
-        signalingThread.join();
-        close(sock);
-        close(server_sock);
-        callee->close();
         std::cout << "[Callee] Shutdown complete." << std::endl;
+    }
 
+private:
+    const int SIGNALING_PORT;
+    rtc::Configuration config;
+    std::shared_ptr<rtc::PeerConnection> pc;
+    std::shared_ptr<rtc::DataChannel> dc;
+
+    std::atomic<bool> connected;
+    std::atomic<bool> dataChannelReady;
+    std::atomic<bool> answerReady;
+
+    int server_sock;
+    int sock;
+
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::thread signalingThread;
+
+    void setupPeerConnection() {
+        pc->onLocalCandidate([this](rtc::Candidate candidate) {
+            sendMessage("candidate:" + std::string(candidate));
+            std::cout << "[Callee] Sent ICE candidate to caller." << std::endl;
+        });
+
+        pc->onStateChange([this](rtc::PeerConnection::State state) {
+            std::cout << "[Callee] State changed: " << stateToString(state) << std::endl;
+            connected = (state == rtc::PeerConnection::State::Connected);
+            cv.notify_all();
+        });
+
+        pc->onLocalDescription([this](rtc::Description description) {
+            sendMessage("answer:" + std::string(description));
+            std::cout << "[Callee] Sent answer SDP to caller." << std::endl;
+            answerReady = true;
+            cv.notify_all();
+        });
+
+        pc->onDataChannel([this](std::shared_ptr<rtc::DataChannel> channel) {
+            std::cout << "[Callee] Data channel received." << std::endl;
+            dc = channel;
+
+            dc->onOpen([this]() {
+                std::cout << "[Callee] Data channel open." << std::endl;
+                dataChannelReady = true;
+                cv.notify_all();
+            });
+
+            dc->onMessage([this](rtc::message_variant msg) {
+                std::visit([this](auto&& data) {
+                    using T = std::decay_t<decltype(data)>;
+                    if constexpr (std::is_same_v<T, std::string>) {
+                        std::cout << "[Callee] Received message: " << data << std::endl;
+                        std::string response = "Callee echo: " + data;
+                        if (dc && dc->isOpen()) {
+                            dc->send(response);
+                            std::cout << "[Callee] Sent transformed message: " << response << std::endl;
+                        }
+                    } else if constexpr (std::is_same_v<T, rtc::binary>) {
+                        std::cout << "[Callee] Received binary message, size: " << data.size() << std::endl;
+                    }
+                }, msg);
+            });
+        });
+    }
+
+    void setupSignalingServer() {
+        server_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_sock < 0) throw std::runtime_error("socket failed");
+
+        sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = htons(SIGNALING_PORT);
+
+        if (bind(server_sock, (sockaddr*)&addr, sizeof(addr)) < 0) throw std::runtime_error("bind failed");
+        if (listen(server_sock, 1) < 0) throw std::runtime_error("listen failed");
+
+        std::cout << "[Callee] Waiting for caller connection..." << std::endl;
+        sock = accept(server_sock, nullptr, nullptr);
+        if (sock < 0) throw std::runtime_error("accept failed");
+        std::cout << "[Callee] Caller connected." << std::endl;
+    }
+
+    void signalingLoop() {
+        std::string msg;
+        while (receiveMessage(msg)) {
+            if (msg.rfind("offer:", 0) == 0) {
+                rtc::Description desc(msg.substr(6), rtc::Description::Type::Offer);
+                pc->setRemoteDescription(desc);
+                std::cout << "[Callee] Received and set offer SDP." << std::endl;
+                pc->setLocalDescription();
+            } else if (msg.rfind("candidate:", 0) == 0) {
+                rtc::Candidate candidate(msg.substr(10));
+                pc->addRemoteCandidate(candidate);
+                std::cout << "[Callee] Received and added ICE candidate." << std::endl;
+            }
+        }
+    }
+
+    void sendMessage(const std::string& msg) {
+        uint32_t len = htonl(msg.size());
+        if (send(sock, &len, sizeof(len), 0) != sizeof(len)) return;
+        if (send(sock, msg.data(), msg.size(), 0) != (ssize_t)msg.size()) return;
+    }
+
+    bool receiveMessage(std::string& out) {
+        uint32_t len = 0;
+        ssize_t n = recv(sock, &len, sizeof(len), MSG_WAITALL);
+        if (n <= 0) return false;
+        len = ntohl(len);
+        out.resize(len);
+        return recv(sock, &out[0], len, MSG_WAITALL) == (ssize_t)len;
+    }
+
+    const char* stateToString(rtc::PeerConnection::State state) const {
+        switch (state) {
+            case rtc::PeerConnection::State::New: return "New";
+            case rtc::PeerConnection::State::Connecting: return "Connecting";
+            case rtc::PeerConnection::State::Connected: return "Connected";
+            case rtc::PeerConnection::State::Disconnected: return "Disconnected";
+            case rtc::PeerConnection::State::Failed: return "Failed";
+            case rtc::PeerConnection::State::Closed: return "Closed";
+            default: return "Unknown";
+        }
+    }
+};
+
+int main() {
+    try {
+        WebRTCCallee callee;
+        callee.run();
     } catch (const std::exception& e) {
         std::cerr << "[Callee] Exception: " << e.what() << std::endl;
         return 1;
--- src/webrtc/caller.cpp
+++ src/webrtc/caller.cpp
@@ -11,43 +11,12 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-const int SIGNALING_PORT = 9000;
+class WebRTCCaller {
+public:
+    WebRTCCaller(const std::string& serverIp, int port)
+        : serverIp(serverIp), port(port), connected(false), dataChannelReady(false), offerReady(false), answerSet(false) {}
 
-const char* stateToString(rtc::PeerConnection::State state) {
-    switch (state) {
-        case rtc::PeerConnection::State::New: return "New";
-        case rtc::PeerConnection::State::Connecting: return "Connecting";
-        case rtc::PeerConnection::State::Connected: return "Connected";
-        case rtc::PeerConnection::State::Disconnected: return "Disconnected";
-        case rtc::PeerConnection::State::Failed: return "Failed";
-        case rtc::PeerConnection::State::Closed: return "Closed";
-        default: return "Unknown";
-    }
-}
-
-// Simple send string over socket with length prefix
-bool sendMessage(int sock, const std::string& msg) {
-    uint32_t len = htonl(msg.size());
-    if (send(sock, &len, sizeof(len), 0) != sizeof(len)) return false;
-    if (send(sock, msg.data(), msg.size(), 0) != (ssize_t)msg.size()) return false;
-    return true;
-}
-
-// Simple receive string from socket with length prefix
-bool receiveMessage(int sock, std::string& out) {
-    uint32_t len = 0;
-    ssize_t n = recv(sock, &len, sizeof(len), MSG_WAITALL);
-    if (n == 0) return false; // connection closed
-    if (n != sizeof(len)) return false;
-    len = ntohl(len);
-    out.resize(len);
-    n = recv(sock, &out[0], len, MSG_WAITALL);
-    if (n != (ssize_t)len) return false;
-    return true;
-}
-
-int main() {
-    try {
+    void run() {
         rtc::InitLogger(rtc::LogLevel::Info);
 
         rtc::Configuration config;
@@ -55,70 +24,89 @@ int main() {
         config.portRangeBegin = 10000;
         config.portRangeEnd = 10100;
 
-        auto caller = std::make_shared<rtc::PeerConnection>(config);
+        peerConnection = std::make_shared<rtc::PeerConnection>(config);
 
-        std::mutex mtx;
-        std::condition_variable cv;
-        std::atomic<bool> connected{false};
-        std::atomic<bool> dataChannelReady{false};
-        std::atomic<bool> offerReady{false};
-        std::atomic<bool> answerSet{false};
+        setupSocket();
+        setupPeerConnection();
 
-        std::queue<rtc::Candidate> candidates;
-        std::mutex candidatesMtx;
+        dataChannel = peerConnection->createDataChannel("test");
+        setupDataChannel();
 
-        // Setup TCP socket client to signaling server (callee listens)
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock < 0) {
-            perror("socket");
-            return 1;
+        peerConnection->setLocalDescription();
+
+        signalingThread = std::thread(&WebRTCCaller::handleSignaling, this);
+
+        waitForConnection();
+
+        if (connected && dataChannelReady) {
+            for (int i = 0; i < 5; i++) {
+                std::string msg = "Hello from caller #" + std::to_string(i);
+                dataChannel->send(msg);
+                std::cout << "[Caller] Sent: " << msg << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
         }
+
+        signalingThread.join();
+        close(sock);
+        peerConnection->close();
+        std::cout << "[Caller] Shutdown complete." << std::endl;
+    }
+
+private:
+    std::string serverIp;
+    int port;
+    int sock;
+    std::shared_ptr<rtc::PeerConnection> peerConnection;
+    std::shared_ptr<rtc::DataChannel> dataChannel;
+    std::atomic<bool> connected;
+    std::atomic<bool> dataChannelReady;
+    std::atomic<bool> offerReady;
+    std::atomic<bool> answerSet;
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::thread signalingThread;
+
+    void setupSocket() {
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) throw std::runtime_error("Failed to create socket");
+
         sockaddr_in addr{};
         addr.sin_family = AF_INET;
-        addr.sin_port = htons(SIGNALING_PORT);
-        inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+        addr.sin_port = htons(port);
+        inet_pton(AF_INET, serverIp.c_str(), &addr.sin_addr);
 
         std::cout << "[Caller] Connecting to signaling server..." << std::endl;
-        if (connect(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
-            perror("connect");
-            return 1;
-        }
+        if (connect(sock, (sockaddr*)&addr, sizeof(addr)) < 0) throw std::runtime_error("Failed to connect");
         std::cout << "[Caller] Connected to signaling server." << std::endl;
+    }
 
-        caller->onLocalCandidate([&](rtc::Candidate candidate) {
-            std::string candStr = std::string(candidate);
-            std::string msg = "candidate:" + candStr;
-            {
-                std::lock_guard<std::mutex> lock(candidatesMtx);
-                candidates.push(candidate);
-            }
-            sendMessage(sock, msg);
-            std::cout << "[Caller] Sent ICE candidate to callee." << std::endl;
+    void setupPeerConnection() {
+        peerConnection->onLocalCandidate([this](rtc::Candidate candidate) {
+            std::string msg = "candidate:" + std::string(candidate);
+            sendMessage(msg);
         });
 
-        caller->onStateChange([&](rtc::PeerConnection::State state) {
+        peerConnection->onStateChange([this](rtc::PeerConnection::State state) {
             std::cout << "[Caller] State changed: " << stateToString(state) << std::endl;
             connected = (state == rtc::PeerConnection::State::Connected);
             cv.notify_all();
         });
 
-        caller->onLocalDescription([&](rtc::Description description) {
-            std::string offerSdp = std::string(description);
-            std::string msg = "offer:" + offerSdp;
-            sendMessage(sock, msg);
-            std::cout << "[Caller] Sent offer to callee." << std::endl;
-            offerReady = true;
-            cv.notify_all();
+        peerConnection->onLocalDescription([this](rtc::Description description) {
+            std::string msg = "offer:" + std::string(description);
+            sendMessage(msg);
         });
+    }
 
-        // Create data channel
-        auto dc = caller->createDataChannel("test");
-        dc->onOpen([&]() {
+    void setupDataChannel() {
+        dataChannel->onOpen([this]() {
             std::cout << "[Caller] Data channel open." << std::endl;
             dataChannelReady = true;
             cv.notify_all();
         });
-        dc->onMessage([](rtc::message_variant msg) {
+
+        dataChannel->onMessage([](rtc::message_variant msg) {
             std::visit([](auto&& data) {
                 using T = std::decay_t<decltype(data)>;
                 if constexpr(std::is_same_v<T, std::string>)
@@ -127,70 +115,62 @@ int main() {
                     std::cout << "[Caller] Received binary message, size: " << data.size() << std::endl;
             }, msg);
         });
+    }
 
-        // Create offer
-        caller->setLocalDescription();
-
-        // Thread to receive signaling messages
-        std::thread signalingThread([&]() {
-            std::string msg;
-            std::atomic<bool> answerSet{false};
-
-            while (receiveMessage(sock, msg)) {
-                if (msg.rfind("answer:", 0) == 0) {
-                    if (!answerSet.exchange(true)) {  // Only set once
-                        std::string sdp = msg.substr(7);
-                        rtc::Description desc(sdp, rtc::Description::Type::Answer);
-                        caller->setRemoteDescription(desc);
-                        std::cout << "[Caller] Received and set answer SDP." << std::endl;
-                    } else {
-                        std::cout << "[Caller] Duplicate answer SDP received, ignoring." << std::endl;
-                    }
-                } else if (msg.rfind("candidate:", 0) == 0) {
-                    std::string candStr = msg.substr(10);
-                    rtc::Candidate candidate(candStr);
-                    caller->addRemoteCandidate(candidate);
-                    std::cout << "[Caller] Received and added ICE candidate." << std::endl;
-                }
-            }
-            
-        });
-
-        // Wait for connection
-        {
-            std::unique_lock<std::mutex> lock(mtx);
-            if (!cv.wait_for(lock, std::chrono::seconds(30), [&]() { return connected.load(); })) {
-                std::cerr << "[Caller] Timeout waiting for connection." << std::endl;
+    void handleSignaling() {
+        std::string msg;
+        while (receiveMessage(msg)) {
+            if (msg.rfind("answer:", 0) == 0 && !answerSet.exchange(true)) {
+                std::string sdp = msg.substr(7);
+                peerConnection->setRemoteDescription(rtc::Description(sdp, rtc::Description::Type::Answer));
+            } else if (msg.rfind("candidate:", 0) == 0) {
+                std::string candStr = msg.substr(10);
+                peerConnection->addRemoteCandidate(rtc::Candidate(candStr));
             }
         }
+    }
 
-        if (connected) {
-            std::cout << "[Caller] Connection established! Waiting for data channel..." << std::endl;
-            {
-                std::unique_lock<std::mutex> lock(mtx);
-                if (!cv.wait_for(lock, std::chrono::seconds(10), [&]() { return dataChannelReady.load(); })) {
-                    std::cerr << "[Caller] Timeout waiting for data channel." << std::endl;
-                }
-            }
-            
-            std::this_thread::sleep_for(std::chrono::seconds(10)); 
+    void waitForConnection() {
+        std::unique_lock<std::mutex> lock(mtx);
+        if (!cv.wait_for(lock, std::chrono::seconds(30), [this] { return connected.load(); }))
+            std::cerr << "[Caller] Timeout waiting for connection." << std::endl;
 
-            if (dataChannelReady) {
-                // Send some test messages
-                for (int i = 0; i < 5; i++) {
-                    std::string msg = "Hello from caller #" + std::to_string(i);
-                    dc->send(msg);
-                    std::cout << "[Caller] Sent: " << msg << std::endl;
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
-                }
-            }
+        if (!cv.wait_for(lock, std::chrono::seconds(10), [this] { return dataChannelReady.load(); }))
+            std::cerr << "[Caller] Timeout waiting for data channel." << std::endl;
+    }
+
+    void sendMessage(const std::string& msg) {
+        uint32_t len = htonl(msg.size());
+        send(sock, &len, sizeof(len), 0);
+        send(sock, msg.data(), msg.size(), 0);
+    }
+
+    bool receiveMessage(std::string& out) {
+        uint32_t len = 0;
+        ssize_t n = recv(sock, &len, sizeof(len), MSG_WAITALL);
+        if (n == 0 || n != sizeof(len)) return false;
+        len = ntohl(len);
+        out.resize(len);
+        return recv(sock, &out[0], len, MSG_WAITALL) == (ssize_t)len;
+    }
+
+    const char* stateToString(rtc::PeerConnection::State state) const {
+        switch (state) {
+            case rtc::PeerConnection::State::New: return "New";
+            case rtc::PeerConnection::State::Connecting: return "Connecting";
+            case rtc::PeerConnection::State::Connected: return "Connected";
+            case rtc::PeerConnection::State::Disconnected: return "Disconnected";
+            case rtc::PeerConnection::State::Failed: return "Failed";
+            case rtc::PeerConnection::State::Closed: return "Closed";
+            default: return "Unknown";
         }
+    }
+};
 
-        signalingThread.join();
-        close(sock);
-        caller->close();
-        std::cout << "[Caller] Shutdown complete." << std::endl;
-
+int main() {
+    try {
+        WebRTCCaller caller("127.0.0.1", 9000);
+        caller.run();
     } catch (const std::exception& e) {
         std::cerr << "[Caller] Exception: " << e.what() << std::endl;
         return 1;
